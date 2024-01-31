6e919022f87b52263563ceec1ed9678468fadda8
sule <inanirsule@gmail.com>
2024-01-31 11:45:54 -0500

    added gitignore

---
+++ src/javaCode1.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class javaCode1 {
+    public class NumberIdentifier {
+
+        public void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.print("Enter a number: ");
+
+            int userInput = scanner.nextInt();
+
+            String result = identify(userInput);
+
+            System.out.println("Result: " + result);
+
+            scanner.close();
+        }
+
+
+        public String identify(int number) {
+            if (number % 2 == 0) {
+                return "Even";
+            } else {
+                return "Odd";
+            }
+        }
+    }
+}
---
+++ src/javaCode3.java
@@ -0,0 +1,19 @@
+public class javaCode3 {
+    public static void main(String[] args) {
+        printNumbers();
+    }
+    public static void printNumbers() {
+        for (int i = 1; i <= 30; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FINRA");
+            } else if (i % 3 == 0) {
+                System.out.println("FIN");
+            } else if (i % 5 == 0) {
+                System.out.println("RA");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+}
+
